4f9cb67759173f5b017d61b7f4cc9cd7a7bc83aa
Kyle Truong <ktruong008@gmail.com>
2024-06-26 15:00:33 -0400

    sidebar

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Builders of stuff";
+export const SITE_DESCRIPTION = "Builders of stuff";
+export const GITHUB_URL = "https://github.com/builders-of-stuff";
+export const TWITTER_URL = "https://twitter.com/kylebuildsstuff";
