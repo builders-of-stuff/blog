1ecaa669b095e5e676644842215aa3e36e61a9e2
Kyle Truong <ktruong008@gmail.com>
2024-06-26 14:22:41 -0400

    Add tailwind

---
+++ src/components/tester.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  const what = "sdadasd";
+</script>
+
+<h1 class="text-3xl font-bold underline text-red-800">
+  hello {what}
+</h1>
+<p class="text-red-900">
+  hello {what}
+</p>
+<p class="text-blue-900">
+  hello {what}
+</p>
